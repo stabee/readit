2a02a775fbf2b6ffaf3711cc36232a138cb0b06f
stabee <stabe42@gmail.com>
2021-01-27 20:26:28 -0800

    Serve static html page for heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const routes = require("./routes");
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -25,6 +26,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api", routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
